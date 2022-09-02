63f990453211b112d043917a671aaef50225fc89
Pavlo Kostiuk <kostyk.pavel.09@gmail.com>
2022-09-03 00:10:01 +0400

    create guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+// Derive the `fmt::Debug` implementation for `Structure`. `Structure`
+// is a structure which contains a single `i32`.
+#[derive(Debug)]
+struct Structure(i32);
+
+// Put a `Structure` inside of the structure `Deep`. Make it printable
+// also.
+#[derive(Debug)]
+struct Deep(Structure);
+
 fn main() {
-    println!("Hello, world!");
+    let animals = vec!["elephant", "cat", "dog"];
+
+    for i in animals.iter() {
+        println!("{}", i)
+    }
 }
